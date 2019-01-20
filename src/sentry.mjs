4b30673eb350c192af013525ceb28508910f5bf9
Dmitry Murzin <diraria@yandex.ru>
2019-01-20 22:51:28 +0300

    log errors captured by sentry in production

--- src/sentry.mjs
+++ src/sentry.mjs
@@ -8,10 +8,8 @@ Sentry.init({
     integrations: [new Sentry.Integrations.Transaction()],
     // https://github.com/getsentry/sentry-javascript/issues/1600#issuecomment-426010114
     beforeSend: (event, hint) => {
-        if (IS_DEVELOPMENT) {
-            console.error(hint.originalException || hint.syntheticException);
-            return null; // this drops the event and nothing will be send to sentry
-        }
-        return event;
+        console.error(hint.originalException || hint.syntheticException);
+        // returning null drops the event, so nothing will be send to sentry in development
+        return IS_DEVELOPMENT ? null : event;
     },
 });
