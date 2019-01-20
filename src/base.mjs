58c474dcd7e2ff53744d21d1ccf036c0658df5fe
Dmitry Murzin <diraria@yandex.ru>
2019-01-20 20:23:09 +0300

    try to handle errors precisely

--- src/base.mjs
+++ src/base.mjs
@@ -1,18 +1,24 @@
 // единственный способ получить нормальные stack trac'ы для async/await методов — настроить babel для преобразования async/await в Promise.then, с последующим замещением глобальной реализации Promise'ов (через `global.Promise = require('bluebird');`)
 // Все остальные способы не работают (https://github.com/nodejs/node/issues/11865)
 
-// looks like it is not necessary:
+// IMPORTANT:
+//  unhandledRejection event is deprecated
+//  (so we must always handle any possible rejection)
+
 // proper logging on error (why node doesn't have it by default???)
-process
-    .on('unhandledRejection', (reason, p) => {
-        console.error('Unhandled Rejection at Promise');
-        if (reason.host) {
-            console.error(reason.host + reason.path);
-        }
-        console.error(reason);
-        process.exit(1);
-    })
-    .on('uncaughtException', err => {
-        console.error(err, 'Uncaught Exception thrown');
-        process.exit(1);
-    });
+// process
+//     .on('unhandledRejection', (reason, p) => {
+//         console.log('unhandledRejection');
+//         console.error('Unhandled Rejection at Promise');
+//         if (reason.host) {
+//             console.error(reason.host + reason.path);
+//         }
+//         console.error(reason);
+//     })
+//     .on('uncaughtException', err => {
+//         console.error(err, 'Uncaught Exception thrown');
+//     });
+
+export function handleReject(reason) {
+    console.error(reason);
+}
--- src/webServer.mjs
+++ src/webServer.mjs
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import express from 'express';
+import router from 'express-promise-router';
 import bodyParser from 'body-parser';
 import GithubWebHook from 'express-github-webhook';
 import main from './main';
@@ -9,47 +10,50 @@ import crowdinApi, { getCrowdinDirectoryName } from './crowdin';
 import github from './github';
 import Raven from 'raven';
 import { getRepositoryLogs } from './console-log-interceptor';
+import { handleReject } from './base';
 
 class WebServer {
     init() {
         const PORT = process.env.PORT || 5000;
-        this.app = express();
-        this.app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-        this.app.use(Raven.requestHandler());
+        const app = express();
+        app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+        this.router = router();
+        this.router.use(Raven.requestHandler());
         this.initRoutes();
-        this.initWebhooks();
-        this.app.use(Raven.errorHandler());
+        this.router.use(Raven.errorHandler());
+        app.use('/webhook', this.getWebhookRouter());
+        app.use('/', this.router);
     }
 
     initRoutes() {
         assert(process.env.WEBSERVER_SECRET);
         const authMiddleware = this.authMiddleware;
 
-        this.app.get('/', this.getMainPage);
-        this.app.get('/updates', this.getUpdates);
-        this.app.get('/logs/\*', this.getRepositoryLogs);
-        this.app.get('/triggerUpdate', authMiddleware, this.triggerUpdate);
-        this.app.get('/deleteCrowdinExampleDirectory', authMiddleware, this.deleteCrowdinExampleDirectory);
-        this.app.get('/repositories', authMiddleware, this.getRepositories);
+        this.router.get('/', this.getMainPage);
+        this.router.get('/updates', this.getUpdates);
+        this.router.get('/logs/\*', this.getRepositoryLogs);
+        this.router.get('/triggerUpdate', authMiddleware, this.triggerUpdate);
+        this.router.get('/deleteCrowdinExampleDirectory', authMiddleware, this.deleteCrowdinExampleDirectory);
+        this.router.get('/repositories', authMiddleware, this.getRepositories);
         this.initExampleRoutes();
     }
 
     // for debug only
     initExampleRoutes() {
-        this.app.get('/error', (req, res) => {
+        this.router.get('/error', (req, res) => {
             console.log('example console log');
             throw Error('Example error');
         });
-        this.app.get('/errorAsync', async (req, res, next) => {
+        this.router.get('/errorAsync', async (req, res) => {
             console.log('before sleep');
             const sleep = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
             await sleep(500);
             console.log('after sleep');
-            try {
-                throw Error('Example async error');
-            } catch (error) {
-                next(error);
-            }
+            throw Error('Example async error');
+        });
+        this.router.get('/errorAsync2', (req, res) => {
+            Promise.reject(Error('Example async2 error')).catch(handleReject);
+            res.send();
         });
     }
 
@@ -62,11 +66,12 @@ class WebServer {
         }
     }
 
-    initWebhooks() {
+    getWebhookRouter() {
         assert(process.env.WEBHOOKS_SECRET);
-        const webhookHandler = GithubWebHook({ path: '/webhook', secret: process.env.WEBHOOKS_SECRET });
-        this.app.use(bodyParser.json());
-        this.app.use(webhookHandler);
+        const router = express.Router();
+        const webhookHandler = GithubWebHook({ path: '/', secret: process.env.WEBHOOKS_SECRET });
+        router.use(bodyParser.json());
+        router.use(webhookHandler);
 
         // webhookHandler.on('*', function (event, repo, data) {
         //     console.log(event, repo, data);
@@ -82,17 +87,19 @@ class WebServer {
 
         webhookHandler.on('installation', (repo, data) => {
             checkRepositorySelection(data);
-            main.onRepositoriesAddedWebhook(data.installation.id, data.repositories);
+            main.onRepositoriesAddedWebhook(data.installation.id, data.repositories).catch(handleReject);
         });
 
         webhookHandler.on('installation_repositories', (repo, data) => {
             checkRepositorySelection(data);
-            main.onRepositoriesAddedWebhook(data.installation.id, data.repositories_added);
+            main.onRepositoriesAddedWebhook(data.installation.id, data.repositories_added).catch(handleReject);
         });
 
         webhookHandler.on('push', (repo, data) => {
-            main.onPushWebhook(data);
+            main.onPushWebhook(data).catch(handleReject);
         });
+
+        return router;
     }
 
     // all next webhooks are for debug
@@ -101,33 +108,26 @@ class WebServer {
         res.send('<p>Factorio mods localization</p><p>See <a href="https://github.com/dima74/factorio-mods-localization">GitHub repository</a> for documentation</p>');
     }
 
-    async getUpdates(req, res, next) {
+    async getUpdates(req, res) {
         res.type('text/plain');
         res.send(await database.getUpdatesInfo());
-        next();
     }
 
     triggerUpdate(req, res) {
         res.type('text/plain').send('Triggered. See logs for details.');
-        main.pushAllCrowdinChangesToGithub();
+        main.pushAllCrowdinChangesToGithub().catch(handleReject);
     }
 
-    async deleteCrowdinExampleDirectory(req, res, next) {
-        try {
-            const crowdinName = getCrowdinDirectoryName('dima74/factorio-mod-example');
-            await crowdinApi.deleteDirectory(crowdinName);
-            res.type('text/plain').send('OK');
-            next();
-        } catch (error) {
-            next(error);
-        }
+    async deleteCrowdinExampleDirectory(req, res) {
+        const crowdinName = getCrowdinDirectoryName('dima74/factorio-mod-example');
+        await crowdinApi.deleteDirectory(crowdinName);
+        res.type('text/plain').send('OK');
     }
 
-    async getRepositories(req, res, next) {
+    async getRepositories(req, res) {
         const repositories = await github.getAllRepositories();
         const response = repositories.map(({ installation, fullName }) => ({ installationId: installation.id, fullName }));
         res.type('text/plain').send(JSON.stringify(response, null, 2));
-        next();
     }
 
     getRepositoryLogs(req, res) {
